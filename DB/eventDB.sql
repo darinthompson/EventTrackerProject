ad634aa2d8a58e4ee9103b8e75782b9262aa35d8
darinthompson <darint07@gmail.com>
2020-06-05 16:44:38 -0600

    Setup DB and stubbed out entity and made sure connection to port 8083 was good

--- DB/eventDB.sql
+++ DB/eventDB.sql
@@ -5,18 +5,15 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mySchema
+-- Schema eventschema
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `mySchema` ;
+DROP SCHEMA IF EXISTS `eventschema` ;
 
 -- -----------------------------------------------------
--- Schema mySchema
+-- Schema eventschema
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mySchema` DEFAULT CHARACTER SET utf8 ;
--- -----------------------------------------------------
--- Schema new_schema1
--- -----------------------------------------------------
-USE `mySchema` ;
+CREATE SCHEMA IF NOT EXISTS `eventschema` DEFAULT CHARACTER SET utf8 ;
+USE `eventschema` ;
 
 -- -----------------------------------------------------
 -- Table `event`
@@ -29,11 +26,24 @@ CREATE TABLE IF NOT EXISTS `event` (
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` VARCHAR(16) NOT NULL,
+  `email` VARCHAR(255) NULL,
+  `password` VARCHAR(32) NOT NULL,
+  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP);
+
 SET SQL_MODE = '';
 DROP USER IF EXISTS user@localhost;
 SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 CREATE USER 'user'@'localhost' IDENTIFIED BY '1qaz!QAZ';
 
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'user'@'localhost';
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
@@ -43,7 +53,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- Data for table `event`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `mySchema`;
+USE `eventschema`;
 INSERT INTO `event` (`id`, `name`) VALUES (1, 'Test');
 
 COMMIT;
