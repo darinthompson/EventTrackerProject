d7da0d1ca663448953003e1ab8dd0a01bd4f3354
darinthompson <darint07@gmail.com>
2020-06-08 10:44:44 -0600

    fixed database issues and added configuration for deploying to ec2

--- DB/libraryDB.sql
+++ DB/libraryDB.sql
@@ -24,12 +24,12 @@ CREATE TABLE IF NOT EXISTS `Book` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(100) NOT NULL,
   `author` VARCHAR(100) NOT NULL,
-  `genre` VARCHAR(100) NULL,
-  `year` INT NULL,
+  `genre` VARCHAR(100) NOT NULL,
+  `year` INT NOT NULL,
   `checked_out` TINYINT NULL,
-  `isbn` VARCHAR(100) NULL,
+  `isbn` VARCHAR(100) NOT NULL,
   `img_url` VARCHAR(250) NULL,
-  `publisher` VARCHAR(250) NULL,
+  `publisher` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -49,12 +49,15 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `librarydb`;
-INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (1, 'Forges of Mars', 'Graham McNeill', 'Sci-Fi', 2018, 0, '978-1-78496-497-9', NULL, 'Black Library');
-INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (2, 'The Shadow Rising', 'Robert Jordan', 'Fantasy', 1992, 0, '978-0-8125-1373-8', NULL, 'Tor');
-INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (3, 'Let\'s Talk Spanish', 'Tracy Van Bischop', 'Language', 2006, 0, '978-1-4351-6059-0', NULL, 'Fall River Press');
-INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (4, 'The Object-Oriented Thought Process', 'Matt Weisfeld', 'Technology/Programming', 2019, 0, '978-0-13-518196-6', NULL, 'Addison-Wesley');
-INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (5, 'The Message: The Bible in Contemporary Language (duoton leatherlike)', 'Eugene H. Peterson', 'Religion', 2016, 0, '978-1-63146-578-9', NULL, 'NavPress');
-INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (6, 'The Holy Bible ESV (navy trutone)', 'Various', 'Religion', 2016, 0, '978-1-4335-6683-7', NULL, 'Crossway');
+INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (2, 'The Shadow Rising', 'Robert Jordan', 'Fantasy', 1992, 0, '978-0-8125-1373-8', 'NULL', 'Tor');
+INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (3, 'Let\'s Talk Spanish', 'Tracy Van Bischop', 'Language', 2006, 0, '978-1-4351-6059-0', 'NULL', 'Fall River Press');
+INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (4, 'The Object-Oriented Thought Process', 'Matt Weisfeld', 'Technology/Programming', 2019, 0, '978-0-13-518196-6', 'NULL', 'Addison-Wesley');
+INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (5, 'The Message: The Bible in Contemporary Language (duoton leatherlike)', 'Eugene H. Peterson', 'Religion', 2016, 0, '978-1-63146-578-9', 'NULL', 'NavPress');
+INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (6, 'The Holy Bible ESV (navy trutone)', 'Various', 'Religion', 2016, 0, '978-1-4335-6683-7', 'NULL', 'Crossway');
+INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (7, 'Forges Of Mars', 'Graham McNeill', 'Sci-Fi', 2018, NULL, '978-1-78496-497-9', 'NULL', 'Black Library');
+INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (8, 'Dungeons And Dragons: Player\'s Handbook (5e)', 'Various', 'RPG/Gaming', 2014, NULL, '978-0-7869-6560-1', 'NULL', 'Wizards Of The Coast');
+INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (9, 'Dungeons And Dragons: Monster Manual (5e)', 'Various', 'RPG/Gaming', 2014, NULL, '978-0-7869-6561-8', 'NULL', 'Wizards Of The Coast');
+INSERT INTO `Book` (`id`, `title`, `author`, `genre`, `year`, `checked_out`, `isbn`, `img_url`, `publisher`) VALUES (10, 'Dungeons And Dragons: Dungeon Master\'s Guide (5e)', 'Various', 'RPG/Gaming', 2014, NULL, '978-0-7869-6562-5', 'NULL', 'Wizards Of The Coast');
 
 COMMIT;
 
--- MyEventTracker/src/main/java/com/skilldistillery/events/MyEventTrackerApplication.java
+++ MyEventTracker/src/main/java/com/skilldistillery/events/MyEventTrackerApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyEventTrackerApplication {
+public class MyEventTrackerApplication extends SpringBootServletInitializer {
+	  
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(MyEventTrackerApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyEventTrackerApplication.class, args);
