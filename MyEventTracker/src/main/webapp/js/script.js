19d26ccd1d86102a70f281ca0c19282194376a7f
darinthompson <darint07@gmail.com>
2020-06-13 17:53:23 -0600

    SLightly buggy, but can get books by id

--- MyEventTracker/src/main/webapp/js/script.js
+++ MyEventTracker/src/main/webapp/js/script.js
@@ -1,15 +1,18 @@
-/**
- * 
- */
-
+//var submitbtn = document.getElementById('submit');
 window.addEventListener('load', function() {
 	init();
 	getAllThebooks();
 });
 
-
 function init() {
 	console.log("script.js loaded");
+	document.bookSearchForm.book.addEventListener('click', function(e) {
+		e.preventDefault();
+		var bookId = document.bookSearchForm.bookId.value;
+		if (!isNaN(bookId) && bookId > 0) {
+			getBookById(bookId);
+		}
+	});	
 }
 
 function showErrors(message) {
@@ -19,13 +22,41 @@ function showErrors(message) {
 	div.appendChild(h3);
 }
 
+function displayBook(book) {
+	console.log("*******" + book);
+	let div = document.getElementById('bookList');
+	div.textContent = '';
+	let h1 = document.createElement('h1');
+	h1.textContent = book.title;
+	div.appendChild(h1);
+}
+
+function getBookById(id) {
+	console.log(id);
+	let xhr = new XMLHttpRequest();
+	let uri = `api/books/${id}`;
+	xhr.open('GET', uri);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				var book = JSON.parse(xhr.responseText);
+				console.log(book);
+				displayBook(book);
+			} else {
+				showErrors("Book not found!");
+			}
+		}
+	}
+	xhr.send(null);
+}
+
 function getAllThebooks() {
 	let xhr = new XMLHttpRequest();
 	let uri = 'api/books';
 	xhr.open('GET', uri);
 	xhr.onreadystatechange = function() {
-		if(xhr.readyState === 4) {
-			if(xhr.status == 200) {
+		if (xhr.readyState === 4) {
+			if (xhr.status == 200) {
 				var books = JSON.parse(xhr.responseText);
 				console.log(books);
 				displayBooks(books);
@@ -38,9 +69,11 @@ function getAllThebooks() {
 }
 
 function displayBooks(books) {
+	console.log("I RAN LIKE I WAS SUPPOSED TO");
+	console.log(books);
 	let div = document.getElementById('bookList');
 	let ul = document.createElement('ul');
-	for(var i = 0; i < books.length; i++) {
+	for (var i = 0; i < books.length; i++) {
 		let li = document.createElement('li');
 		let author = document.createElement('blockquote');
 		let title = document.createElement('h3');
